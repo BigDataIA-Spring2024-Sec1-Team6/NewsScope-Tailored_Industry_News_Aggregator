b7cc1bfb8d35f8a692368f0209f0279b95b76b7e
smithiParthi213 <145076623+smithiParthi213@users.noreply.github.com>
2024-04-26 13:24:48 -0400

    Create stg_cnn.sql

---
+++ Scripts/stg_cnn.sql
@@ -0,0 +1,9 @@
+select
+    headings as headings,
+    --links as links,
+    content as content,
+    industry as industry,
+    entry_date as entry_date,
+    image as image
+
+from RAW_NEWSFEED.cnn.cnn_newsfeed
